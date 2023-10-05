2155465765388b32e99abcbfcd2dd8d3d9873b6e
JavaConterry <conterry05@gmail.com>
2023-10-05 19:36:12 +0200

    saving csv to dictionary data structure

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,14 +1,15 @@
 from urllib import request 
 import re
 from datetime import datetime
-import sys
 import os
 import pandas as pd
+import anima
 
-animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", 
-            "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+# csv_filename_format = "DATA\\NOAA_data_province_ID={}_time={}.csv"
+# datetime_format = "%d-%m-%Y_%H_%M_%S"
 
-def get_province_data(proovince_id = 1):
+#TODO check if data is already exists
+def __request_province_data(proovince_id = 1):
     url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?country=UKR&provinceID="+str(proovince_id)+"&year1=1981&year2=2023&type=Mean"
     wp = request.urlopen(url)
     data = wp.read()
@@ -31,16 +32,33 @@ def save_all_province_datas(folder_name="DATA"):
         os.makedirs(folder_name)
 
     for i in range(1, 28):
-        write_province_data_into_file((get_province_data(i), str(i)), folder_name=folder_name)
-        sys.stdout.write("\r" + animation[i*len(animation)//28])
-        sys.stdout.flush
+        write_province_data_into_file((__request_province_data(i), str(i)), folder_name=folder_name)
+        anima.update(i)
     print("Files are saved!")
     print("Saving process time: ", datetime.now()-saving_time)
 
-# def make_df():
-#     headers = ['Year', 'Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI']
+def __get_id_from_name(str_name):
+    if("ID=" not in str_name):
+        print("Corrupted name given get_id_from_name() ...\n     in ... get_df_from_files()")
+        return
+    id = re.search("ID=(.*)_time", str_name).group(1)
+    return int(id)
+
+# headers = ['Year', 'Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI']
+def get_df_from_files(folder_name="DATA"):
+    saved_csvs = dict()
+    for file_name in os.listdir(folder_name):
+        if(".csv" in file_name):
+            print(file_name)
+            df = pd.read_csv(folder_name+"\\"+file_name)
+            saved_csvs[__get_id_from_name(file_name)] = df
+    return(saved_csvs)
+
+    
 
 
 
 folder_name="DATA"
-save_all_province_datas(folder_name)
+if(folder_name not in os.listdir() or len(os.listdir(folder_name)) == 0):
+    save_all_province_datas(folder_name="DATA")
+print(get_df_from_files()[1])
