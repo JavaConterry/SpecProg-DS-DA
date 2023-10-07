622333ab2fa08e9f6b7027a219bac0bbf9d0c5dd
JavaConterry <conterry05@gmail.com>
2023-10-07 13:32:44 +0200

    notebook, cleaning, VHI processing modules, debuging

--- Lab2/README.md
+++ Lab2/README.md
@@ -2,3 +2,4 @@
 >>In requirements.txt file are writen all required libs
 >> simplply use *pip install -r requirements.txt* on the environment you use
 
+The lab2.py is a project for researching VHI indices data collected and calculated by www.star.nesdis.noaa.gov
--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,15 +1,13 @@
 from urllib import request 
-import re
 from datetime import datetime
-import os
 import pandas as pd
-import anima
+import re
+import os
+from settings import change_map
+from settings import header
 
-# csv_filename_format = "DATA\\NOAA_data_province_ID={}_time={}.csv"
-# datetime_format = "%d-%m-%Y_%H_%M_%S"
 
-#TODO check if data is already exists
-def __request_province_data(proovince_id = 1):
+def request_province_data(proovince_id = 1):
     url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?country=UKR&provinceID="+str(proovince_id)+"&year1=1981&year2=2023&type=Mean"
     wp = request.urlopen(url)
     data = wp.read()
@@ -19,6 +17,7 @@ def __request_province_data(proovince_id = 1):
     data = re.sub(r'<.*?>', '', data)   # regex to getting rid of html tags
     return data
 
+
 def write_province_data_into_file(str_csv_data_provinceID, folder_name="DATA"):
     data, num = str_csv_data_provinceID
     if(type(str_csv_data_provinceID)!=tuple):
@@ -26,33 +25,35 @@ def write_province_data_into_file(str_csv_data_provinceID, folder_name="DATA"):
         return
     open(folder_name+"\\NOAA_data_province_ID="+str(num)+"_time="+datetime.now().strftime("%d-%m-%Y_%H_%M_%S")+".csv", 'w').write(data)
 
+
 def save_all_province_datas(folder_name="DATA"):
     saving_time = datetime.now()
     if not os.path.isdir(folder_name):
         os.makedirs(folder_name)
 
     for i in range(1, 28):
-        write_province_data_into_file((__request_province_data(i), str(i)), folder_name=folder_name)
-        anima.update(i)
+        write_province_data_into_file((request_province_data(i), str(i)), folder_name=folder_name)
     print("Files are saved!")
     print("Saving process time: ", datetime.now()-saving_time)
 
-def __get_id_from_name(str_name):
+
+def get_id_from_name(str_name):
     if("ID=" not in str_name):
         print("Corrupted name given get_id_from_name() ...\n     in ... get_df_from_files()")
         return
     id = re.search("ID=(.*)_time", str_name).group(1)
     return int(id)
 
-# headers = ['Year', 'Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI']
+
 def get_df_from_files(folder_name="DATA"):
     saved_csvs = dict()
     for file_name in os.listdir(folder_name):
         if(".csv" in file_name):
-            print(file_name)
-            df = pd.read_csv(folder_name+"\\"+file_name)
-            saved_csvs[__get_id_from_name(file_name)] = df
+            df = pd.read_csv(folder_name+"\\"+file_name, usecols = ['year', 'week', ' SMN', 'SMT', 'VCI', 'TCI', ' VHI'])
+            df.columns = header
+            saved_csvs[get_id_from_name(file_name)] = df
     return(saved_csvs)
+
 
 def change_indices(dict_to_change, change_layout):
     if(type(change_layout) is not dict):
@@ -61,61 +62,47 @@ def change_indices(dict_to_change, change_layout):
     for key in dict_to_change:
         rewritten_dict[change_layout[key]] = dict_to_change[key]
     return rewritten_dict
-    
-def calculate_VHI(data_frames):
-    begin = sorted(list(data_frames.keys()))[0]
-    for df_ind in range(begin, len(data_frames)+begin):
-        data_frames[df_ind][" VHI"] = (data_frames[df_ind]["VCI"]+data_frames[df_ind]["TCI"])/2
-    return data_frames
 
 
-
-folder_name="DATA"
-if(folder_name not in os.listdir() or len(os.listdir(folder_name)) == 0):
-    save_all_province_datas(folder_name="DATA")
-print(get_df_from_files().keys())
-get_df_from_files()
+def VHI_extr(df):
+    vhi_df = df[['Year','VHI']]
+    return(vhi_df, min(vhi_df['VHI']), max(vhi_df['VHI']))
 
 
-change_map = {
-    1: 22,
-    2: 24,
-    3: 23,
-    4: 25,
-    5: 3,
-    6: 4,
-    7: 8,
-    8: 19,
-    9: 20,
-    10: 21,
-    11: 9,
-    12: 26, #kyiv - 26
-    13: 10,
-    14: 11,
-    15: 12,
-    16: 13,
-    17: 14,
-    18: 15,
-    19: 16,
-    20: 27, #sevastopol - 27
-    21: 17,
-    22: 18,
-    23: 6,
-    24: 1,
-    25: 2,
-    26: 7,
-    27: 5
-}
+def VHI_drought_above(df, min_VHI):
+    return df[(min_VHI<df["VHI"]) & (df["VHI"]<40)][["Year", "VHI"]].reset_index(drop=True)
 
 
-dfs = get_df_from_files()
-new_dfs = change_indices(dfs, change_map)
-calculated_dfs = calculate_VHI(new_dfs)
+def moderate_VHI_below(df, max_VHI):
+    return df[(60<df["VHI"]) & (df["VHI"]<max_VHI)][["Year","VHI"]].reset_index(drop=True)
 
 
-# handling missing data TODO: clean this mess
-begin = sorted(list(calculated_dfs.keys()))[0]
-for df_ind in range(begin, len(calculated_dfs)+begin):
-    calculated_dfs[df_ind] = calculated_dfs[df_ind].drop(
-        calculated_dfs[df_ind].loc[calculated_dfs[df_ind][" VHI"]<=-0.9].index)
-print(calculated_dfs)
+def lab2_task_folower():
+    folder_name="DATA"
+    if(folder_name not in os.listdir() or len(os.listdir(folder_name)) == 0):
+        save_all_province_datas(folder_name="DATA")
+    dfs = get_df_from_files()
+    print(dfs[1].columns)
+    print(dfs.keys())
+    print(dfs[1])
+    dfs = change_indices(dfs, change_map)
+
+    #missed data handling
+    begin = sorted(list(dfs.keys()))[0]
+    for df_ind in range(begin, len(dfs)+begin):
+        dfs[df_ind] = dfs[df_ind].drop(
+            dfs[df_ind].loc[dfs[df_ind]["VHI"]<=-0.9].index)
+    print(dfs[1])
+
+    #min max VHI
+    vhi_1, minv, maxv = VHI_extr(dfs[1])
+    print("min_vhi: ", minv, "\n","max_vhi: ", maxv)
+    print(vhi_1)
+
+    #drough VHI
+    cond_VHI = VHI_drought_above(dfs[1], 30)
+    print(cond_VHI)
+
+    #moderate VHI
+    moderate_VHI = moderate_VHI_below(dfs[1], 80)
+    print(moderate_VHI)
