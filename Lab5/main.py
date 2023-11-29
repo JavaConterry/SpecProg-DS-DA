959cafd0feaf32fabb31f29c2b584c544c4708ef
JavaConterry <conterry05@gmail.com>
2023-11-29 09:30:18 +0100

    noise update function

--- Lab5/main.py
+++ Lab5/main.py
@@ -3,13 +3,11 @@ from matplotlib.widgets import Slider, CheckButtons, Button
 import numpy as np
 from scipy.signal import iirfilter, lfilter
 
+
+
 def harmonic_with_noise(amplitude, frequency, phase, noise_mean, noise_covariance, show_noise):
-    time = np.arange(0, 360)
     harmonic = amplitude * np.sin(frequency * time + phase)
-    if show_noise:
-        noise = np.random.normal(noise_mean, noise_covariance, size=len(time))
-        harmonic += noise
-    return time, harmonic
+    return harmonic
 
 init_amplitude = 1.0
 init_frequency = 0.1
@@ -17,13 +15,15 @@ init_phase = 0
 noise_mean = 0
 noise_covariance = 0.1
 show_noise = False
+time = np.arange(0, 360)
+noise = np.random.normal(noise_mean, noise_covariance, size=len(time))
 
 fig, axs = plt.subplots(nrows=1, ncols=2)
 axs[0].set_title('Harmonic Signal')
 axs[1].set_title('Filtered Harmonic Signal')
 plt.subplots_adjust(bottom=0.50)
 
-time, harmonic = harmonic_with_noise(init_amplitude, init_frequency, init_phase, noise_mean, noise_covariance, show_noise)
+harmonic = harmonic_with_noise(init_amplitude, init_frequency, init_phase, noise_mean, noise_covariance, show_noise)
 line, = axs[0].plot(time, harmonic, color='green')
 
 filtered_harmonic = np.copy(harmonic)
@@ -42,15 +42,18 @@ ax_show_noise = plt.axes([0.2, 0.09, 0.3, 0.04])
 show_noise_checkbox = CheckButtons(ax_show_noise, ['Show noise'])
 
 reset_button = Button(plt.axes([0.53, 0.09, 0.3, 0.04]), "Reset")
-iirf_button = Button(plt.axes([0.2, 0.04, 0.3, 0.04]), "IIR filter")
+# iirf_button = Button(plt.axes([0.2, 0.04, 0.3, 0.04]), "IIR filter")
 
 def update_graph(val):
+    global show_noise
     amplitude = amplitude_slider.val
     frequency = frequency_slider.val
     phase = phase_slider.val
     show_noise = show_noise_checkbox.get_status()[0]
     global harmonic
-    time, harmonic = harmonic_with_noise(amplitude, frequency, phase, noise_mean, noise_covariance, show_noise)
+    harmonic = harmonic_with_noise(amplitude, frequency, phase, noise_mean, noise_covariance, show_noise)
+    if(show_noise):
+        harmonic+=noise
     global filtered_harmonic                            #added to update the filtered graph everytime anything has been changed
     b, a = iirfilter(3, 0.5, btype='low')               #
     filtered_harmonic = lfilter(b, a, harmonic)         #
@@ -64,20 +67,20 @@ def reset_init_vals(val):
     phase_slider.reset()
     show_noise_checkbox.set_active(False)
 
-# def apply_iirfilter(val):
-#     global filtered_harmonic
-#     b, a = iirfilter(3, 0.5, btype='low')
-#     filtered_harmonic = lfilter(b, a, harmonic)
-#     line2.set_ydata(filtered_harmonic)
-#     fig.canvas.draw_idle()
+def update_noise(val):
+    global show_noise
+    show_noise = not show_noise
+
+def update_noise_and_graph(val):
+    update_noise(val)
+    update_graph(val)
+    
 
 amplitude_slider.on_changed(update_graph)
 frequency_slider.on_changed(update_graph)
 phase_slider.on_changed(update_graph)
-show_noise_checkbox.on_clicked(update_graph)
+show_noise_checkbox.on_clicked(update_noise_and_graph)
 reset_button.on_clicked(reset_init_vals)
-# iirf_button.on_clicked(apply_iirfilter)
-iirf_button.on_clicked(update_graph)
 
 # Display the plot
 plt.legend()
