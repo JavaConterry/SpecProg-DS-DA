78697226690050dae617efa27474e7fe94984697
JavaConterry <conterry05@gmail.com>
2023-12-09 17:31:51 +0100

    noise fix

--- Lab6/data_generator.py
+++ Lab6/data_generator.py
@@ -1,15 +1,7 @@
 import numpy as np
 
 def create_set_near_lin_func(k, b): #function coefitients f(x)=kx+b
-    x, y = np.random.randint(100, high=5000, size=3000), np.random.randint(100, high=100000, size=3000)
-    indices_to_delete = []
-    def f(x):
-        return k*x + b
-
-    for i in range(len(x)):
-        if abs(f(x[i]) - y[i]) > 5000:
-            indices_to_delete.append(i)
-
-    x = np.delete(x, indices_to_delete)
-    y = np.delete(y, indices_to_delete)
+    x = np.linspace(0, 100, 1000)
+    print(x.shape) #mean = 0
+    y = k*x+b + np.random.normal(0, 100, 1000) 
     return x, y
