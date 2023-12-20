c58788bc53276de7df99b704a6c90c446b4adeec
JavaConterry <conterry05@gmail.com>
2023-12-20 20:29:43 +0100

    2nd part

--- Course Project/pyc.py
+++ Course Project/pyc.py
@@ -15,11 +15,11 @@ print(str(df[['category']]).split("|"))
 
 
 
-df = df.fillna(method="bfill")
+df = df.dropna()
 
 num_df = df.iloc[:, 2:8]
-spec_idx = num_df[num_df['rating'] == "|"].index[0]
-num_df.iloc[spec_idx] = num_df.iloc[spec_idx-1]
+spec_idx = num_df[num_df['rating'] == "|"].index
+num_df.drop(index=spec_idx, inplace=True)
 
 
 def parse_cash(value):
@@ -45,42 +45,44 @@ num_df['rating_count'] = num_df['rating_count'].apply(parse_to_int)
 num_df['rating'] = num_df['rating'].apply(parse_to_flt)
 
 
-print('=========================Before standartisation=====================')
-print(num_df.head(2))
-print(num_df.mean())
 
 
-def mean(df_col):
-    return df_col.sum()/len(df_col)
-def standart_diviation(df_col):   # <--- Dispersion
-    mean_f = mean(df_col)
-    sum_of_the_squered_differences = 0
-    for i in range(len(df_col)):
-        sum_of_the_squered_differences += (df_col[i]-mean_f)**2
-    return math.sqrt(sum_of_the_squered_differences/len(df_col))
-def standartise(df_column):
-    return (df_column-mean(df_column)) / standart_diviation(df_column)
-
-std_num_df = num_df.copy()
-std_num_df ['discounted_price'] = standartise(num_df['discounted_price'])
-std_num_df ['actual_price'] = standartise(num_df['actual_price'])
-std_num_df ['rating'] = standartise(num_df['rating'])
-std_num_df ['discount_percentage'] = standartise(num_df['discount_percentage'])
-std_num_df ['rating_count'] = standartise(num_df['rating_count'])
-
-print('=========================After standartisation=====================')
-print(std_num_df.head(2))
-print(std_num_df.mean())
+# print('=========================Before standartisation=====================')
+# print(num_df.head(2))
+# print(num_df.mean())
 
 
+# def mean(df_col):
+#     return df_col.sum()/len(df_col)
+# def standart_diviation(df_col):   # <--- Dispersion
+#     mean_f = mean(df_col)
+#     sum_of_the_squered_differences = 0
+#     for i in range(len(df_col)):
+#         sum_of_the_squered_differences += (df_col[i]-mean_f)**2
+#     return math.sqrt(sum_of_the_squered_differences/len(df_col))
+# def standartise(df_column):
+#     return (df_column-mean(df_column)) / standart_diviation(df_column)
 
-scaler = StandardScaler()
+# std_num_df = num_df.copy()
+# std_num_df ['discounted_price'] = standartise(num_df['discounted_price'])
+# std_num_df ['actual_price'] = standartise(num_df['actual_price'])
+# std_num_df ['rating'] = standartise(num_df['rating'])
+# std_num_df ['discount_percentage'] = standartise(num_df['discount_percentage'])
+# std_num_df ['rating_count'] = standartise(num_df['rating_count'])
 
-scaler_num_df = pd.DataFrame(scaler.fit_transform(num_df.iloc[:,1:]), columns=num_df.columns[1:])
+# print('=========================After standartisation=====================')
+# print(std_num_df.head(2))
+# print(std_num_df.mean())
 
-print('=========================After scaler=====================')
-print(scaler_num_df.head(2))
-print(scaler_num_df.mean())
+
+
+# scaler = StandardScaler()
+
+# scaler_num_df = pd.DataFrame(scaler.fit_transform(num_df.iloc[:,1:]), columns=num_df.columns[1:])
+
+# print('=========================After scaler=====================')
+# print(scaler_num_df.head(2))
+# print(scaler_num_df.mean())
 
 
 
